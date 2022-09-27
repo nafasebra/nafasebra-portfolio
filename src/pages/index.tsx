91b2fd3092c9a80c6259cfa009ce271f41a0d92d
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-27 19:35:19 +0330

    Add index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import React from "react";
+
+function index() {
+  return (
+    <>
+      <Head>
+        <title>Nafas Ebrahimi</title>
+        <meta name="keywords" content="nafasebra, nafasebrahimi, nafas ebrahimi, Nafas Ebrahimi, Frontend, Front-end, frontend dev, frontend developer" />
+        <meta name="description" content="My name is Nafas Ebrahimi and a Front-end developer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        The First Page!
+      </main>
+    </>
+  );
+}
+
+export default index;
