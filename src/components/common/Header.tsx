a85aa732a6f149bdcade3b753a101a78d3c0d5ac
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-09-30 21:35:33 +0330

    Change content of header section: Write persion anything

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className='flex flex-col-reverse lg:flex-row items-center justify-between py-10'>
+    <header className='flex flex-col-reverse lg:flex-row items-center justify-between py-20'>
       <div className='w-[90%] lg:w-[48%]'>
-        <p className='text-xl text-light'>Hello, I am</p>
-        <h1 className='text-5xl text-orange font-bold py-3'>Nafas Ebrahimi</h1>
-        <p className='text-light'>Front-end developer</p>
+        <p className='text-xl text-light'>سلام به همگی. بنده</p>
+        <h1 className='text-5xl text-orange font-bold py-5'>نفس ابراهیمی</h1>
+        <p className='text-light text-xl'>یک برنامه نویس فرانت اند</p>
         <p className='pt-5 text-sm leading-6 text-gray-400'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et officiis modi quod consectetur tenetur, ex ipsa delectus? Vitae asperiores harum nostrum velit ad excepturi facere. Odio cumque quia saepe. Dolore.
+          و همچنین کسی که یادگیری, یاددادن و خلق چیز ها براش تفریحه و سعی میکنه کمکی باشه برای جامعه امروز. کارتیمی براش جذابه و دوست داره از بقیه چه کوچیکتر یا چه بزرگتر تجربه کسب کنه...
         </p>
       </div>
       <div className='w-[90%] lg:w-[48%]'></div>
