e54825457d130dc833e8f8548cc75d52e1701acd
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-01 23:34:55 +0330

    Add other skills to skill section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,15 @@ function index() {
               <div className="py-3 px-4 text-light rounded-lg bg-dark-200">HTML</div>
               <div className="py-3 px-4 text-light rounded-lg bg-dark-200">CSS</div>
               <div className="py-3 px-4 text-light rounded-lg bg-dark-200">JavaScript</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">TailwindCSS</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">Bootstrap</div>
               <div className="py-3 px-4 text-light rounded-lg bg-dark-200">SASS/LESS</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">ReactJS</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">Redux(toolkit)</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">TypeScript</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">NPM/Yarn</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">Git</div>
+              <div className="py-3 px-4 text-light rounded-lg bg-dark-200">NextJS</div>
             </div>
           </Container>
         </section>
