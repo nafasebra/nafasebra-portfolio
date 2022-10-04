d8d2ea01439d73dd6b8a6e5b5cdd5c7869899013
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-04 12:54:37 +0330

    include shape to footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import DarkSection from "../layout/DarkSection";
+import arrowDashed from "../../assets/images/arrow-dashed.svg";
+import Image from 'next/image'
 
 function Footer() {
   return (
@@ -7,7 +9,8 @@ function Footer() {
       <DarkSection>
         <div className="flex flex-col items-center">
           <h2 className="text-gray-300 pb-10 font-bold">با من درارتباط باش...</h2>
-          <ul className="flex items-center">
+          <Image src={arrowDashed} className="py-5 w-10" alt="" />
+          <ul className="flex items-center pt-10">
             {/* the linkedin link */}
             <li className="ml-5">
               <a
