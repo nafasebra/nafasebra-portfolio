c3d2a4c9bd080303649358104fc71b1da72bcd4d
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-05 21:23:15 +0330

    Fix error about stroke linerjoin property

--- src/components/ui/SocialSection.tsx
+++ src/components/ui/SocialSection.tsx
@@ -17,7 +17,7 @@ function SocialSection() {
             stroke="currentColor"
             fill="none"
             strokeLinecap="round"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <rect x="4" y="4" width="16" height="16" rx="2" />
@@ -42,7 +42,7 @@ function SocialSection() {
             stroke="currentColor"
             fill="none"
             strokeLinecap="round"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
@@ -52,7 +52,7 @@ function SocialSection() {
       {/* the youtube link */}
       {/* <li className='ml-5'>
             <a href="#" className='block text-light p-1 hover:text-orange cursor-pointer'>
-              <svg xmlns="http://www.w3.org/2000/svg" className='w-7 h-7 transition-none' viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" stroke-linejoin="round">
+              <svg xmlns="http://www.w3.org/2000/svg" className='w-7 h-7 transition-none' viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <rect x="3" y="5" width="18" height="14" rx="4" />
                 <path d="M10 9l5 3l-5 3z" />
@@ -74,7 +74,7 @@ function SocialSection() {
             stroke="currentColor"
             fill="none"
             strokeLinecap="round"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <polyline points="3 9 12 15 21 9 12 3 3 9" />
