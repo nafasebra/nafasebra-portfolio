44c1d705a2928227aa749d9134f393165294068e
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-05 23:54:10 +0330

    Changes

--- src/pages/api/contactus.ts
+++ src/pages/api/contactus.ts
@@ -1,4 +1,7 @@
 // TODO: this file will be edit and to change types
 export default function (req: any, res: any) {
+  const nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({});
+  
   console.log(req.body);
 }
