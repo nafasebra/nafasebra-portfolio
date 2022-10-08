bd3ef732f4b7c25b5de96fbf1c88e9a80dfdd8a3
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-08 13:36:14 +0330

    Create splashscreen component file

---
+++ src/components/common/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SplashScreen() {
+  return (
+    <div className='fixed inset-0 w-screen h-screen flex items-center justify-center z-30 visible opacity-100 bg-dark-100' id='splashScreen'>
+      <div></div>
+    </div>
+  )
+}
+
+export default SplashScreen
