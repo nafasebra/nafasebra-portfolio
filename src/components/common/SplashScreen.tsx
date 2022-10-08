ef8a96419515c12e94b2b0035378ac15298d87a9
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-08 20:02:03 +0330

    Made loading component

--- src/components/common/SplashScreen.tsx
+++ src/components/common/SplashScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Loading from '../ui/Loading'
 
 function SplashScreen() {
   return (
     <div className='fixed inset-0 w-screen h-screen flex items-center justify-center z-30 visible opacity-100 bg-dark-100' id='splashScreen'>
-      <div></div>
+      <Loading />
     </div>
   )
 }
---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='w-10 h-10 bg-orange overflow-hidden rounded-full animation-loading'>
+      
+    </div>
+  )
+}
+
+export default Loading
