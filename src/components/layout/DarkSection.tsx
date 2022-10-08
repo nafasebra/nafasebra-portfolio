5e605a493bf40485d4d3b98d00c5dd1a51a5ae5e
Nafiseh Ebrahimi <ebranafas@gmail.com>
2022-10-08 21:49:37 +0330

    Handle id props to darksection

--- src/components/layout/DarkSection.tsx
+++ src/components/layout/DarkSection.tsx
@@ -3,11 +3,12 @@ import Container from "./Container";
 
 type PropType = {
   children: JSX.Element[] | JSX.Element;
+  id: string;
 };
 
-function DarkSection({ children }: PropType) {
+function DarkSection({ children, id }: PropType) {
   return (
-    <section className="bg-dark-200 py-20">
+    <section className="bg-dark-200 py-20" id={id}>
       <Container>{children}</Container>
     </section>
   );
