1ac8802470b9a7020ef4ed871d3215aacc52b3c8
unknown <ebranafas@gmail.com>
2022-10-22 20:06:55 +0330

    Change skills section direction

--- src/components/common/Skills.tsx
+++ src/components/common/Skills.tsx
@@ -14,84 +14,84 @@ import npmIcon from "../../assets/icons/npm.svg";
 
 function Skills() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 font-poppins">
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">HTML</span>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 font-poppins" dir="ltr">
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={htmlIcon} alt="html icon" />
         </div>
+        <span className="ml-3">HTML</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">CSS</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={cssIcon} alt="css icon" />
         </div>
+        <span className="ml-3">CSS</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">JavaScript</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={jsIcon} alt="js icon" />
         </div>
+        <span className="ml-3">JavaScript</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
+        <div className="w-6 h-6 flex justify-center">
+          <Image src={cssIcon} alt="css icon" />
+        </div>
         <span className="ml-3">TailwindCSS</span>
+      </div>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={cssIcon} alt="css icon" />
         </div>
-      </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
         <span className="ml-3">Bootstrap</span>
-        <div className="w-6 h-6 flex justify-center">
-          <Image src={cssIcon} alt="css icon" />
-        </div>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">SASS/LESS</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={sassIcon} alt="sass icon" />
         </div>
+        <span className="ml-3">SASS/LESS</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
+        <div className="w-6 h-6 flex justify-center">
+          <Image src={reactIcon} alt="react js icon" />
+        </div>
         <span className="ml-3">ReactJS</span>
+      </div>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={reactIcon} alt="react js icon" />
         </div>
-      </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
         <span className="ml-3">Redux(toolkit)</span>
-        <div className="w-6 h-6 flex justify-center">
-          <Image src={reactIcon} alt="react js icon" />
-        </div>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">TypeScript</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={tsIcon} alt="typescript icon" />
         </div>
+        <span className="ml-3">TypeScript</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">NPM/Yarn</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={npmIcon} alt="npm icon" />
         </div>
+        <span className="ml-3">NPM/Yarn</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">Git</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={gitIcon} alt="git icon" />
         </div>
+        <span className="ml-3">Git</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">NextJS</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={reactIcon} alt="react js icon" />
         </div>
+        <span className="ml-3">NextJS</span>
       </div>
-      <div className="flex items-center justify-end py-3 px-4 text-light rounded-lg bg-dark-200">
-        <span className="ml-3">AdobeXD/Figma</span>
+      <div className="flex items-center justify-start py-3 px-4 text-light rounded-lg bg-dark-200">
         <div className="w-6 h-6 flex justify-center">
           <Image src={figmaIcon} alt="figma icon" />
         </div>
+        <span className="ml-3">AdobeXD/Figma</span>
       </div>
     </div>
   );
