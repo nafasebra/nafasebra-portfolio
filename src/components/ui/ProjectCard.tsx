fe7a9830a419a804a7cd96ff5864444858538054
unknown <ebranafas@gmail.com>
2022-10-23 11:54:13 +0330

    Style: change some styles of project carrd

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -12,7 +12,7 @@ function ProjectCard(props: PropType) {
   const { name, description, imageUrl, target } = props;
 
   return (
-    <div className="relative overflow-hidden h-[300px] group rounded-lg">
+    <div className="relative overflow-hidden h-[300px] group rounded-lg transition-all">
       <Image
           src={imageUrl}
           layout="fill"
@@ -22,14 +22,14 @@ function ProjectCard(props: PropType) {
       <div className="flex flex-col justify-end p-5 bg-gradient-to-t from-dark-100 via-dark-100/75 to-dark-100/30 absolute inset-0">
         <div>
           <p className="text-orange text-lg font-bold py-3">{name}</p>
-          <div className="flex flex-col items-start justify-center h-[0] group-hover:h-[100px] overflow-hidden">
+          <div className="flex flex-col items-start justify-center md:h-[0] md:group-hover:h-[100px] overflow-hidden">
             <p className="text-light text-sm pb-5">{description || name}</p>
             <a
               href={target}
               className="flex items-center text-light hover:text-orange hover:underline hover:underline-offset-8"
             >
               <span className="ml-2">مشاهده سایت</span>
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16l-4-4m0 0l4-4m-4 4h18" /></svg>
+              <svg className="w-5 h-5 transition-none" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16l-4-4m0 0l4-4m-4 4h18" /></svg>
             </a>
           </div>
         </div>
