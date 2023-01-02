05f10f2b01bfea23508d55186c079c006401f34b
unknown <ebranafas@gmail.com>
2023-01-02 22:17:07 +0330

    resolve some bugs

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import DarkSection from "../layout/DarkSection";
-import arrowDashed from "../../assets/images/arrow-dashed.svg";
 import Image from 'next/image'
 import SocialSection from "../ui/SocialSection";
 
@@ -10,7 +9,11 @@ function Footer() {
       <DarkSection>
         <div className="flex flex-col items-center">
           <h2 className="text-gray-300 pb-10 font-bold">با من در شبکه های اجتماعی درارتباط باش...</h2>
-          <Image src={arrowDashed} className="py-5 w-10" alt="" />
+          <div className="w-16 h-[100px] mx-auto">
+            <div className="w-full h-full relative">
+              <Image src="/assets/images/arrow-dashed.svg" layout="fill" alt="" />
+            </div>
+          </div>
           <SocialSection />
         </div>
       </DarkSection>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header id='header' className='flex flex-col-reverse lg:flex-row items-center justify-between pt-10 pb-20'>
       <div className='w-[90%] lg:w-[48%]'>
-        <p className='text-sm text-gray-300'>سلام به همگی. <span className='text-orange font-bold'>بنده</span></p>
+        <p className='text-sm text-gray-300'>سلام به همگی. <span className='text-orange font-bold'>من</span></p>
         <h1 className='text-5xl text-orange font-bold py-5'>نفس ابراهیمی</h1>
         <p className='text-gray-200 text-xl'> برنامه نویس <span className='text-light font-bold'>فرانت اند وب</span></p>
         <p className='pt-5 text-sm leading-6 text-gray-400'>
@@ -18,7 +18,7 @@ function Header() {
       </div>
       <div className='w-[90%] h-full lg:w-[48%] mb-5 md:mb-0'>
         <div className='w-[90%] h-[650px] relative object-contain mx-auto'>
-          <Image layout="fill" alt="nafasebra homepage" src="/assets/images/Homepage.png" />
+          <Image layout="fill" alt="nafasebra homepage" src="/assets/images/homepage.png" />
         </div>
       </div>
     </header>
