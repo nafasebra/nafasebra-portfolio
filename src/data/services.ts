69a36014aca401af15bb00d75524219e1370b56f
unknown <ebranafas@gmail.com>
2023-01-23 10:15:23 +0330

    create json file for services info

---
+++ src/data/services.ts
@@ -0,0 +1,18 @@
+export type ServiceType = {
+   title: string;
+   description: string;
+ };
+ 
+ export const ServicesData: ServiceType[] = [
+   {
+     title: "پیاده سازی قالب سایت",
+     description: "برای انجام پروژه میتونید روی من حساب کنید",
+   }, {
+      title: "توسعه پروژه های React و NextJS",
+      description: "برای انجام پروژه میتونید روی من حساب کنید",
+   }, {
+      title: "تدریس خصوصی",
+      description: "برای انجام پروژه میتونید روی من حساب کنید",
+   },
+ ];
+ 
