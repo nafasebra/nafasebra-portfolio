f12a41de8b6f8a59a988fb402379fd434e0a12e3
unknown <ebranafas@gmail.com>
2023-02-08 22:53:44 +0330

    change services section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ function index(props: PropType) {
             </h3>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
             {ServicesData.map((item, index) => (
               <ServiceCard
                 key={index}
