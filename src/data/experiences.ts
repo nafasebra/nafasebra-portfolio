1c647dc372d68e523939aa35d453665b0c98e96b
unknown <ebranafas@gmail.com>
2023-02-16 12:07:13 +0330

    add uuid to experience data file

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -1,4 +1,7 @@
+import {v4 as uuidv4} from 'uuid'
+
 export type ExperienceType = {
+   id: string;
    title: string;
    link: string;
    dateFrom: string;
@@ -8,6 +11,7 @@ export type ExperienceType = {
 
 export const Experiences: ExperienceType[] = [
    {
+      id: uuidv4(),
       title: 'شرکت Asrez',
       link: 'https://asrez.com',
       dateFrom: '10 دی 1399',
@@ -15,6 +19,7 @@ export const Experiences: ExperienceType[] = [
       description: ['پیاده سازی قالب های سایت با HTML, CSS و Vanilla JS', 'پیاده سازی پروژه با ReactJS و NextJS', 'تجربه طراحی UI قالب سایت های شرکت استیل صنعت کاشان, اسرز و میرسه'],
    }, 
    {
+      id: uuidv4(),
       title: 'شرکت شمیران وب',
       link: 'https://shemiranweb.com',
       dateFrom: '15 آبان 1401',
