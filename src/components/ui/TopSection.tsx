b3e97e29818d9915e79bc772ba7e2cd7ec973833
Nafas Ebrahimi <ebranafas@gmail.com>
2023-12-06 12:14:28 +0330

    style: change style of top section

--- src/components/ui/TopSection.tsx
+++ src/components/ui/TopSection.tsx
@@ -9,8 +9,8 @@ function TopSection(props: PropType) {
   const { title, subTitle } = props;
 
   return (
-    <div className="pb-16">
-      <p className="flex items-center text-orange pb-5">
+    <div className="pb-16 space-y-3">
+      <p className="flex items-center text-orange font-bold">
         <span className="block ml-2 w-7 h-[3px] bg-orange"></span>
         {title}
       </p>
